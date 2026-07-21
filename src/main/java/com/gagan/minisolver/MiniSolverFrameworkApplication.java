fbb0e8f41183e483c11089230b510126f9c61c90
gagan052 <gagansaini7207@gmail.com>
2026-07-21 12:23:34 +0530

    Project setup and architecture skeleton

---
+++ src/main/java/com/gagan/minisolver/MiniSolverFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.gagan.minisolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSolverFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSolverFrameworkApplication.class, args);
+	}
+
+}
